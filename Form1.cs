6781d9efa6cc2ac2081e143efb80bd0b2a45bb27
wangwor <wang.t.h.f.g@gmail.com>
2020-01-13 11:10:23 +0800

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HtmlElement
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            string agreement = System.Environment.CurrentDirectory+@"\html";
+            string com = Path.Combine(agreement, "PickAgreementContent.html");
+            webBrowser1.Navigate(com);
+        }
+       
+    }
+}
